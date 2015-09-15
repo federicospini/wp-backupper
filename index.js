83a3b0a1e133488e54fa02519664d19164d7d48b
Federico Spini <federico.spini@gmail.com>
2015-09-15 17:12:12 +0200

    added crarl backup

--- index.js
+++ index.js
@@ -1,118 +1,68 @@
 require('dotenv').load();
-// var Browser = require('zombie');
-// var request = require('request');
-// var fs = require('fs');
-// var async = require('async');
-
-// var urlLogin = 'http://www.sitac.it/wp-login.php?redirect_to=http%3A%2F%2Fwww.sitac.it%2Fwp-admin%2F&reauth=1';
-// var urlExport = 'http://www.sitac.it/wp-admin/export.php?download=true&content=all&cat=0&post_author=0&post_start_date=0&post_end_date=0&post_status=0&page_author=0&page_start_date=0&page_end_date=0&page_status=0&submit=Scarica+file+di+esportazione';
-// var urlInspira = 'http://www.sitac.it?feed=redux_options_theme_option&secret=dbd2cc0714365cdc87b4dbdf41a91250'
-// var urlSlider = 'http://www.sitac.it/wp-admin/admin-ajax.php?action=revslider_ajax_action&client_action=export_slider&dummy=false&nonce=448d26a050&sliderid=1'
-// var user = process.env.USERNAME;
-// var pass = process.env.PASSWORD;
-
-// var browser = new Browser();
-
-// browser.visit(urlLogin, login);
-
-// function login (err) {
-//   if (err) {
-//     console.log(err);
-//     return;
-//   }
-    
-//   browser
-//     .fill('#user_login', user)
-//     .fill('#user_pass', pass)
-
-//   browser.pressButton('#wp-submit', handleCookies);
-// }
-
-// function handleCookies (err) {
-//   if (err) {
-//     console.log(err);
-//     return;
-//   }
-
-//   var cookies = browser.cookies.select({domain: 'www.sitac.it'});
-//   var cookieJar = request.jar();
-
-//   saveURL(urlExport, cookies, cookieJar, './data/export.xml', function (resp) {
-//     console.log('\n\n\tDONE :)\n\n');
-//     process.exit(0);
-//   });
-
-
-//   // request({
-//   //   url:urlExport,
-//   //   jar: j})
-//   // .on('end', function(response) {
-//   //   console.log('\n\n\tDONE :)\n\n');
-//   //   process.exit(0);
-//   // })
-//   // .pipe(fs.createWriteStream('./data/export.xml'));  
-
-  
-// }
-
-// function saveURL(url, cookies, cookieJar, filepath, done) {
-//   bakeCookies(cookies, cookieJar);
-//   sendRequest(url, cookieJar, filepath, done);
-// }
-
-// function bakeCookies (cookies, cookieJar) {
-//   browser.cookies.forEach(function (cookie) {
-//     cookieJar.setCookie(request.cookie(cookie.toString()), urlExport);
-//   });
-// }
-
-// function sendRequest (url, cookieJar, filepath, done) {
-//   request({
-//     url:url,
-//     jar: cookieJar})
-//   .on('end', done)
-//   .pipe(fs.createWriteStream(filepath));  
-// }
-
-// function getDate () {
-//   var date = new Date();
-//   var month = date.getUTCMonth() + 1; //months from 1-12
-//   var day = date.getUTCDate();
-//   var year = date.getUTCFullYear();
-
-//   month = month.length === 2 ? month : '0' + month;
-//   day = day.length === 2 ? day : '0' + day;
-
-//   return year + "-" + month + "-" + day;
-// }
-
+var cron = require('cron');
+var async = require('async');
 
 ///
 
 var Backupper = require('./lib/index.js');
 
-var urls = [{
-  filename: 'wp-export.xml',
-  url: 'http://www.sitac.it/wp-admin/export.php?download=true&content=all&cat=0&post_author=0&post_start_date=0&post_end_date=0&post_status=0&page_author=0&page_start_date=0&page_end_date=0&page_status=0&submit=Scarica+file+di+esportazione',
-  desc: 'site content'
-}, {
-  filename: 'inspira-options.json',
-  url: 'http://www.sitac.it?feed=redux_options_theme_option&secret=dbd2cc0714365cdc87b4dbdf41a91250',
-  desc: 'inspira options'
-}, {
-  filename: 'rev-slider.zip',
-  url: 'http://www.sitac.it/wp-admin/admin-ajax.php?action=revslider_ajax_action&client_action=export_slider&dummy=false&nonce=448d26a050&sliderid=1',
-  desc: 'revolutionary slider'
-}];
+var urls_sitac = [
+  {
+    filename: 'wp-export.xml',
+    url: 'http://www.sitac.it/wp-admin/export.php?download=true&content=all&cat=0&post_author=0&post_start_date=0&post_end_date=0&post_status=0&page_author=0&page_start_date=0&page_end_date=0&page_status=0&submit=Scarica+file+di+esportazione',
+    desc: 'site content'
+  }, {
+    filename: 'inspira-options.json',
+    url: 'http://www.sitac.it?feed=redux_options_theme_option&secret=dbd2cc0714365cdc87b4dbdf41a91250',
+    desc: 'inspira options'
+  }
+];
 
-var options = {
+var options_sitac = {
   domain: 'www.sitac.it',
+  // path: '~/backups/crarl.it/',
   path: './data/',
-  urls: urls,
+  urls_c: urls_sitac,
   login: {
     url: 'http://www.sitac.it/wp-login.php?redirect_to=http%3A%2F%2Fwww.sitac.it%2Fwp-admin%2F&reauth=1',
     username: process.env.USERNAME,
-    password: process.env.PASSWORD
+    password: process.env.SITAC_PASSWORD
+  },
+  rsync: {
+    url: 'sitac.it',
+    username: process.env.SSH_USERNAME, // id_rsa.pub of the source host must be present in host .ssh/authorized_keys
+    source: '/var/lib/docker/vfs/dir/cc6cc5ea79df5edc31f9f7047cb81613b4c57677645087f1c0d7a4721ba335b0/' // wp-content path
+    // source: '/home/admin/www/'
+  }
+};
+
+var urls_crarl = [
+  {
+    filename: 'wp-export.xml',
+    url: 'http://www.crarl.it/wp-admin/export.php?download=true&content=all&cat=0&post_author=0&post_start_date=0&post_end_date=0&post_status=0&page_author=0&page_start_date=0&page_end_date=0&page_status=0&submit=Scarica+file+di+esportazione',
+    desc: 'site content'
+  }, {
+    filename: 'inspira-options.json',
+    url: 'http://www.crarl.it?feed=redux_options_theme_option&secret=dbd2cc0714365cdc87b4dbdf41a91250',
+    desc: 'inspira options'
+  }
+];
+
+var options_crarl = {
+  domain: 'www.crarl.it',
+  // path: '~/backups/sitac.it/',
+  path: './data/',
+  urls: urls_crarl,
+  login: {
+    url: 'http://www.crarl.it/wp-login.php?redirect_to=http%3A%2F%2Fwww.crarl.it%2Fwp-admin%2F&reauth=1',
+    username: process.env.USERNAME,
+    password: process.env.CRARL_PASSWORD
+  },
+  rsync: {
+    url: 'crarl.it',
+    username: process.env.SSH_USERNAME, // id_rsa.pub of the source host must be present in host .ssh/authorized_keys
+    source: '/var/lib/docker/vfs/dir/94a686878887b8413090a58067c7d8cde5760df539f0fb1804b0a70b7cbf1e91/'  // wp-content path
+    // source: '/home/admin/www/'
   }
 };
 
@@ -125,10 +75,51 @@ var end = function (err) {
 
   console.log('Everything is fine ;)');
   process.exit(0);
-}
+};
 
-var b = new Backupper(options);
-// b.run(end);
-b.runFoultTolerant(end);
+var backup_sitac = function (done) {
+  var sitac_b = new Backupper(options_sitac);
+  sitac_b.runFoultTolerant(done);
+};
+
+var backup_crarl = function (done) {
+  var crarl_b = new Backupper(options_crarl);
+  crarl_b.runFoultTolerant(done);
+};
+
+async.waterfall([
+  backup_sitac,
+  backup_crarl
+], end);
+
+// var CronJob = require('cron').CronJob;
+// var sitac_job = new CronJob({
+//   cronTime: '00 30 23 * * 7',
+//   onTick: backup_sitac,
+//   start: false,
+//   timeZone: "Europe/Rome"
+// });
+// job.start();
 
 
+
+/** REV SLIDER SAVING CONFIG **/
+
+// , {
+//     filename: 'rev-slider.zip',
+//     url: 'http://www.sitac.it/wp-admin/admin-ajax.php?action=revslider_ajax_action&client_action=export_slider&dummy=false&nonce=03be557911&sliderid=1',
+//     qs: {
+//       'action': 'revslider_ajax_action',
+//       'client_action': 'export_slider',
+//       'dummy': false,
+//       'nonce': 'c8b0f30cd7',
+//       'sliderid': 1
+//     },
+//     headers: {
+//       'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+//       'Accept-Encoding': 'gzip, deflate, sdch',
+//       'Upgrade-Insecure-Requests': 1,
+//       'Referer': 'http://www.sitac.it/wp-admin/admin.php?page=revslider'
+//     },
+//     desc: 'revolutionary slider'
+//   }
